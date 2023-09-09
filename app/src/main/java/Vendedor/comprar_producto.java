c32155bd5636c77a9a8eaed6be5b410c1f94c0fd
axel030705 <michelcrack03@gmail.com>
2023-09-08 23:09:01 -0600

    Dando detalles a las activitys

--- app/src/main/java/Vendedor/comprar_producto.java
+++ app/src/main/java/Vendedor/comprar_producto.java
@@ -1,14 +1,16 @@
 package Vendedor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.agenda.R;
@@ -26,6 +28,9 @@ public class comprar_producto extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comprar_producto);
 
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        overridePendingTransition(0, R.anim.slide_up);
+
         productoImg= getIntent().getStringExtra("productoImg");
         productoNombre= getIntent().getStringExtra("productoNombre");
         productoPrecio= getIntent().getStringExtra("productoPrecio");
@@ -38,19 +43,46 @@ public class comprar_producto extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, OpcionesCantidad);
         Cantidad.setAdapter(adapter);
 
+        // Agrega el listener para el AutoCompleteTextView
+        Cantidad.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Llama a cargar_producto cuando el usuario selecciona una cantidad
+                cargar_producto();
+            }
+        });
+
+        // Llama a cargar_producto inicialmente (puedes omitir esta línea si no deseas cargarlo al inicio)
         cargar_producto();
     }
 
     @SuppressLint("SetTextI18n")
-    public void cargar_producto(){
+    public void cargar_producto() {
+        // Obtiene la cantidad seleccionada del AutoCompleteTextView
+        String cantidadSeleccionada = Cantidad.getText().toString();
 
+        // Verifica si la cantidad seleccionada es un número válido
+        int cantidad = 0;
+        try {
+            cantidad = Integer.parseInt(cantidadSeleccionada);
+        } catch (NumberFormatException e) {
+
+        }
+
+        // Calcula el precio total multiplicando la cantidad por el precio unitario
+        double precioUnitario = Double.parseDouble(productoPrecio);
+        double precioTotal = cantidad * precioUnitario;
+
+        // Establece el texto del botón con el precio total
+        Btn_finalizarProducto.setText("Comprar MX $" + precioTotal);
+
+        // Actualiza las vistas del nombre y el precio del producto
         textNombreProducto2.setText(productoNombre);
         textPrecioProducto2.setText("MX $" + productoPrecio);
+
+        // Carga la imagen del producto
         Glide.with(imgProducto2.getContext())
                 .load(productoImg)
                 .into(imgProducto2);
-
-
     }
-
 }
--- app/src/main/java/Vendedor/vista_producto.java
+++ app/src/main/java/Vendedor/vista_producto.java
@@ -7,6 +7,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,6 +17,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.agenda.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import org.w3c.dom.Text;
 
@@ -23,6 +27,7 @@ public class vista_producto extends AppCompatActivity {
     public ImageView imgProducto;
     public String productoImg, productoNombre, productoDescripcion, productoPrecio, productoExtra;
     public Button Btn_comprarProducto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,7 +44,6 @@ public class vista_producto extends AppCompatActivity {
         productoDescripcion= getIntent().getStringExtra("productoDescripcion");
         productoPrecio= getIntent().getStringExtra("productoPrecio");
         productoExtra= getIntent().getStringExtra("productoExtra");
-
         CargarProducto();
 
     }
@@ -57,14 +61,70 @@ public class vista_producto extends AppCompatActivity {
 
     }
 
-    public void comprar_producto(View view){
+    /*public void comprar_producto(View view){
 
         Intent intent = new Intent(this, comprar_producto.class);
         intent.putExtra("productoNombre", productoNombre);
         intent.putExtra("productoPrecio", productoPrecio);
         intent.putExtra("productoImg", productoImg);
         startActivity(intent);
+    } */
 
+
+    // Método para mostrar el BottomSheetDialog
+    public void mostrarDetallesDelProducto(View view) {
+        // Crea una instancia del BottomSheetDialog
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        // Infla el diseño de tu BottomSheet personalizado
+        View bottomSheetView = getLayoutInflater().inflate(R.layout.bottom_sheet_layout, null);
+
+        // Configura los elementos del BottomSheet según sus IDs
+        ImageView imgProducto2 = bottomSheetView.findViewById(R.id.imgProducto2);
+        TextView textNombreProducto2 = bottomSheetView.findViewById(R.id.textNombreProducto2);
+        TextView textPrecioProducto2 = bottomSheetView.findViewById(R.id.textPrecioProducto2);
+        AutoCompleteTextView cantidad = bottomSheetView.findViewById(R.id.cantidad2);
+        Button Btn_finalizarProducto2 = bottomSheetView.findViewById(R.id.Btn_finalizarProducto2);
+        cantidad.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Obtiene la cantidad seleccionada del AutoCompleteTextView
+                String cantidadSeleccionada = cantidad.getText().toString();
+
+                // Verifica si la cantidad seleccionada es un número válido
+                int cantidad = 0;
+                try {
+                    cantidad = Integer.parseInt(cantidadSeleccionada);
+                } catch (NumberFormatException e) {
+
+                }
+
+                // Calcula el precio total multiplicando la cantidad por el precio unitario
+                double precioUnitario = Double.parseDouble(productoPrecio);
+                double precioTotal = cantidad * precioUnitario;
+
+                // Establece el texto del botón con el precio total
+                Btn_finalizarProducto2.setText("Comprar MX $" + precioTotal);
+            }
+        });
+
+
+
+        //Aquí puedes cargar la imagen del producto si está disponible:
+        Glide.with(imgProducto2.getContext()).load(productoImg).into(imgProducto2);
+
+        // Asigna valores a los elementos según los datos del producto
+        textNombreProducto2.setText(productoNombre);
+        textPrecioProducto2.setText("MX $" + productoPrecio);
+
+        // Aquí debes configurar las opciones de cantidad y un ArrayAdapter
+        String[] opcionesCantidad = {"1", "2", "3", "4", "5"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item, opcionesCantidad);
+        cantidad.setAdapter(adapter);
+
+        bottomSheetDialog.setContentView(bottomSheetView);
+        // Muestra el BottomSheet
+        bottomSheetDialog.show();
     }
 
 }
